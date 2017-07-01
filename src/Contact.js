b98e8cd4b3a675717df9bfe77128376fb3a37c63
J.C. Francis III <jamescecilfrancis@gmail.com>
2017-07-01 18:24:53 -0500

    Change Style
    Changed style  of contact form

--- src/Contact.js
+++ src/Contact.js
@@ -43,6 +43,11 @@ class Contact extends Component {
   //   })
   // }
 
+  flip() {
+    var s = 'wnzrfprpvysenapvf@tznvy.pbz';
+    return s.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})
+  }
+
   handleChange(model) {
     this.setState({model})
 
